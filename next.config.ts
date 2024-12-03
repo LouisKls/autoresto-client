30909024fddcc3b025e5bc008159c173f7c19c59
Louis Calas <louiskls.dev@gmail.com>
2024-12-03 17:48:09 +0100

    Update Project init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(process.cwd(), 'src/styles')],
+    },
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.join(process.cwd(), 'src'),
+            '@components': path.join(process.cwd(), 'src/components'),
+            '@styles': path.join(process.cwd(), 'src/styles'),
+            '@utils': path.join(process.cwd(), 'src/utils'),
+            '@hooks': path.join(process.cwd(), 'src/hooks'),
+            '@types': path.join(process.cwd(), 'src/types'),
+        }
+        return config
+    },
+}
 
-export default nextConfig;
+export default nextConfig
---
+++ src/app/test/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+const TestPage = () => {
+    return <div style={{ display: 'flex', justifyContent: 'center' }}></div>;
+};
+
+export default TestPage;
